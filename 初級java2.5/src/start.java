815c144bfec958ac04898b6e98606c21632eb990
rui <rui101304@gmail.com>
2024-05-13 12:06:28 +0900

    初級java2.5

---
+++ 初級java2.5/src/start.java
@@ -0,0 +1,20 @@
+
+public class start {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		
+		MyFrame frame3=new RectFrame();
+		frame3.setLocation(50,300);
+		
+		MyFrame frame4=new RectFrame();
+		frame4.setLocation(300,300);
+		
+		int x=2;
+		int y=3;
+		
+		System.out.println(x*y);
+
+	}
+
+}
