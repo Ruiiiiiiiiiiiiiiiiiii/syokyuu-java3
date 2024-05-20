b5b661efec1ac08dd02b6e30829d4b2620adfa58
rui <rui101304@gmail.com>
2024-05-20 10:30:28 +0900

    no message

--- 初級java2.5/src/RectFrame.java
+++ 初級java2.5/src/RectFrame.java
@@ -1,13 +1,7 @@
 
-public class RectFrame extends MyFrame
-{
-	public void run() {
-		fillRect(50, 50, 100 ,100);
-	}
 public class RectFrame extends MyFrame{
 	public void run() {
-		fillRect(50,50,100,100);
-	}
+		fillRect(50, 50, 100 ,100);
 
->>>>>>> main
+	}
 }
