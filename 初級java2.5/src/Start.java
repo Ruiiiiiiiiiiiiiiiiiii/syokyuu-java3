3d0b8fd3de22e11336825483adfb6436c1f9f67d
rui <rui101304@gmail.com>
2024-05-21 11:56:16 +0900

    java5,1

--- 初級java2.5/src/Start.java
+++ 初級java2.5/src/Start.java
@@ -5,15 +5,7 @@ public class Start {
 		// TODO 自動生成されたメソッド・スタブ
 	
 		System.out.println("プログラムを実行します");
-		MyFrame frame1 = new RectTest();
-		MyFrame frael=new RectTest();
-		MyFrame frame3=new RectFrame();
-		frame3.setLocation(50,300);
-		MyFrame fram4l=new OvalTest();
-		MyFrame frame2=new TextTest();
-		MyFrame frame5=new ManyFigures();
-		MyFrame framel=new RectLoop();
-		MyFrame frame6=new RectIf();
+		MyFrame framel = new DisplayRectsSlow();
 		
 	}
 
