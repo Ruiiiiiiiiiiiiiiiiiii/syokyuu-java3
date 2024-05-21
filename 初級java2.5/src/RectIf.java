17272ab392af1bf3bf1856d54c5673d895be10af
rui <rui101304@gmail.com>
2024-05-21 11:45:08 +0900

    追加課題その1

--- 初級java2.5/src/RectIf.java
+++ 初級java2.5/src/RectIf.java
@@ -1,18 +1,14 @@
 
 public class RectIf extends MyFrame {
 	public void run() {
-		int x=30;
-		int c=0;
+		int x=20;
+		int c=15;
 		int i;
+		setColor(0,128,0);
 		for (i=0 ; i<9 ; i++) {
-			setColor(c,c,c);
-			if(i==0) {
-				fillRect(x,80,10,100);
-			}else {
-				fillRect(x,80,10,100);
-			}
-			x+=20;
-			c+=20;
+			fillRect(x,x,c,c);
+			x+=c;
+			c+=15;
 		}
 	}
 
