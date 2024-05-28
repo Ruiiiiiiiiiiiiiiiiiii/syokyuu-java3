64349d7fd5294c70c671c05b4fd1d78f1a44206e
rui <rui101304@gmail.com>
2024-05-28 09:37:59 +0900

    java

--- 初級java2.5/src/Animation.java
+++ 初級java2.5/src/Animation.java
@@ -3,30 +3,39 @@ public class Animation extends MyFrame {
 	public void run() {
 		int y = 100;
 		int x = 30;
-		int i=0;
-		for(i=0; i<3 ; ++i) {
-			while (x <= 170) {
+		int i = 0;
+		while (true) {
+			while (x <= 200) {
 				clear();
 
 				setColor(0, 128, 0);
 				fillRect(x, y, 10, 100);
-				x+=5;				
+				x += 5;
 
 				sleep(0.1);
 			}
-			while (x >= 30) {
+			while (y <= 150) {
+				clear();
+
+				setColor(0, 128, 0);
+				fillRect(x, y, 10, 100);
+				y += 5;
+
+				sleep(0.1);
+			}
+			while (y >= 30) {
 				clear();
 
 				setColor(0, 128, 0);
 				fillRect(x, y, 10, 100);
 
-				x -= 5;
+				y += 5;
 
 				sleep(0.1);
 			}
+
 		}
-			
-		
+
 	}
 
 }
