c5f1f53493ee8a6bc445618ff644c6ff5e637a5e
rui <rui101304@gmail.com>
2024-05-28 10:43:47 +0900

    java6-4

--- 初級java2.5/src/DisplayFaceObject.java
+++ 初級java2.5/src/DisplayFaceObject.java
@@ -1,18 +1,16 @@
 
 public class DisplayFaceObject extends MyFrame{
 	public void run() {
-		Face facel= new Face(50,50);
+		Face facel= new Face(50,50,10,5);
 		
-		Face face2=new Face(200,100);
+		Face face2=new Face(200,100,-10,-5);
 
 		for (int i=0; i<30; i++) {
 			clear();
 			facel.draw(this);
 			face2.draw(this);
-			facel.x+=10;
-			facel.y+=5;
-			face2.x-=10;
-			face2.y-=5;
+			facel.move();
+			face2.move();
 			sleep(0.1);
 		}
 	}
