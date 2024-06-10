119a3688165af00e16d77b9a121631b713fa7b0a
rui <rui101304@gmail.com>
2024-06-10 11:33:58 +0900

    java8-4

---
+++ 初級java2.5/src/DisplayVehicleVector.java
@@ -0,0 +1,9 @@
+import java.util.Vector;
+public class DisplayVehicleVector extends MyFrmae {
+	public void run() {
+		Vector<Vehicle> vehicles=new Vector<Vehicle>();
+		vehicles.add(new Car(10,30,3,0) );
+		
+	}
+
+}
